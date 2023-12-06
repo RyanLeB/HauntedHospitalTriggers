ed314071b2b980f75c3b3a09ac66e990d0811b17
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-12-06 18:14:24 -0400

    small changes

--- Assets/Scripts/TriggerTimeline.cs
+++ Assets/Scripts/TriggerTimeline.cs
@@ -5,11 +5,19 @@ using UnityEngine.Playables;
 
 public class TriggerScript : MonoBehaviour
 {
+
+    
     public bool PlayTimeline = false;
     public PlayableDirector TriggerTimeline;
 
     private void OnTriggerEnter(Collider other)
     {
         TriggerTimeline.Play();
+            
     }
+
+        
+            
+
+   
 }
